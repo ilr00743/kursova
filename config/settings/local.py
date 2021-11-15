a1e76b1ac5b596dcd88575bed93599a99d2eefb3
ilr00743 <ilr00743@gmail.com>
2021-11-15 23:41:14 +0200

    initial commit

--- config/settings/local.py
+++ config/settings/local.py
@@ -3,13 +3,13 @@ from .base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR/'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
 ALLOWED_HOSTS = ['*']
-STATIC_ROOT = BASE_DIR/'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEBUG = True
 STATIC_URL = '/static/'
-MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
