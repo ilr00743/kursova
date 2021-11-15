a5d4072359d2175d084e3e488a8c95517cf20e8f
ilr00743 <ilr00743@gmail.com>
2021-11-15 23:36:58 +0200

    initial commit

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR/'db.sqlite3',
     }
 }
 
@@ -11,5 +11,5 @@ ALLOWED_HOSTS = ['*']
 STATIC_ROOT = BASE_DIR/'static'
 DEBUG = True
 STATIC_URL = '/static/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR/'media'
 MEDIA_URL = '/media/'
