65a119cc630359f233b03ed7d0242aae70e549c5
ilr00743 <ilr00743@gmail.com>
2021-11-15 13:15:08 +0200

    Initial commit

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,13 +3,13 @@ from .base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'illiaR$db_gradebook',
-        'HOST': 'illiaR.mysql.pythonanywhere-services.com',
-        'USER': 'illiaR',
-        'PASSWORD': '1agbdlcid!',
+        'NAME': f'{env("ACC_NAME")}$db_gradebook',
+        'HOST': f'{env("ACC_NAME")}.mysql.pythonanywhere-services.com',
+        'USER': env('ACC_NAME'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
     }
 }
 
-ALLOWED_HOSTS = ['illiaR.pythonanywhere.com']
-STATIC_ROOT = '/home/illiaR/static/'
+ALLOWED_HOSTS = [f'{env("ACC_NAME")}.pythonanywhere.com']
+STATIC_ROOT = f'/home/{env("ACC_NAME")}/static/'
 DEBUG = False
